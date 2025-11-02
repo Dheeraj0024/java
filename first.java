d9335c9a4d0b35332d0a823c5fc8d0dc9d19fd1b
dheeraj0024 <dheeerajawagune0001@gmail.com>
2025-11-02 18:06:50 +0530

    new

--- first.java
+++ first.java
@@ -1,6 +1,10 @@
 public class first {
-    public static void main(String[] args) {
-        System.out.print("hello , world ! ");
+    public static void main(String [] args ){
+        System.out.println("Dheeraj awagune");
+        int a = 10;
+        int b = 20;
+       int c = a * b ;
+       System.out.println(c);
 
     }
 }
